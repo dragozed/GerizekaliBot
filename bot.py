9bf70ff8d7414da532ec431b5828e61ff34679db
Kaan İlik <alperkaanilik@gmail.com>
2021-04-25 11:18:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import discord
 import time
 import random
+import numpy as np
 
 my_secret = os.environ['DISCORD_BOT_TOKEN']
 client = discord.Client()
@@ -35,13 +36,27 @@ async def on_message(message):
         await message.channel.send("Winner is:"+str(duelist1))
       if roll2>roll1:
         await message.channel.send("Winner is: "+str(duelist2))
+      if roll1==roll2:
+        await message.channel.send("Draw ")
+
       f = open("data.txt", "a+") #open txt file append only, meaning: Open the file for writing. The data being written will be inserted at the end, after the existing data. 
       f.write("Winner is:"+str(duelist1)+"; ")
       print(f.read())
       f.close()
       duelBit= False
       
-
+    if message.content.startswith("Total Score"):
+      x= message.author
+      global duelistDatabase= np.zeros(100)
+      n= 0
+      while n<100:
+        if duelistDatabase[n]== x:
+          break
+        if n== 99:
+          n=0; break
+        n= n+1
+      global scoreDatabase
+      await message.channel.send("Total wins are: "+str(scoreDatabase[n]))
     
     
     
